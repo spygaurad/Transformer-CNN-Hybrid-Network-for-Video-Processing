fa6510265c63e97e9789a80d51bc0276ff099b85
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-01-02 13:30:38 +0545

    now makes the directory automatically for the dataset

--- Datasets/generateDataset.py
+++ Datasets/generateDataset.py
@@ -1,28 +1,37 @@
 import cv2
 import os
+from tqdm import tqdm
 
-path = 'Datasets/'
-files = os.listdir(path)
-files = [x for x in files if x[-1]=="4"]
-save_path = "Datasets/Driving Dataset/train_images/image"
-save_path2 = "Datasets/Driving Dataset/test_images/image"
+try:
+    os.makedirs("Datasets/Driving_Dataset/train_images")
+    os.makedirs("Datasets/Driving_Dataset/test_images")
+except :
+    pass
 
-i = 0
-j = 0
+finally:
+    path = 'Datasets/'
+    files = os.listdir(path)
+    files = [x for x in files if x[-1]=="4"]
+    print(files)
+    save_path = "Datasets/Driving_Dataset/train_images/image"
+    save_path2 = "Datasets/Driving_Dataset/test_images/image"
+
+    i = 0
+    j = 0
 
 
-for file in files:
-    video = cv2.VideoCapture(f'{path}{file}')
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_counter = 0
-    while frame_counter < total_frames:
-        _, frame = video.read()
-        frame_counter += 1
-        frame = cv2.resize(frame, (1280, 720))
-        if frame_counter%10 == 0:
-            cv2.imwrite(f'{save_path2}_{j}.png', frame)
-            j+=1
-        else:
-            cv2.imwrite(f'{save_path}_{i}.png', frame)
-            i += 1
+    for file in tqdm(files):
+        video = cv2.VideoCapture(f'{path}{file}')
+        total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_counter = 0
+        while frame_counter < total_frames:
+            _, frame = video.read()
+            frame_counter += 1
+            frame = cv2.resize(frame, (1280, 720))
+            if frame_counter%10 == 0:
+                cv2.imwrite(f'{save_path2}_{j}.png', frame)
+                j+=1
+            else:
+                cv2.imwrite(f'{save_path}_{i}.png', frame)
+                i += 1
     
