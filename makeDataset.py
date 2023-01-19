061da764e1045f436c77eb4030e0195bc19d2d7e
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-01-19 16:57:09 +0545

    solved the dataset

--- makeDataset.py
+++ makeDataset.py
@@ -29,50 +29,92 @@ import shutil
 '''Now we make the folder structure similar to what we are about to do below and for the further tasks that we're going to do in the future'''
 
 
-source_directory = 'Datasets/VOS/train/train/JPEGImages/'
-destination_directory = 'Datasets/VOS/train/images/'
-os.makedirs(destination_directory)
-subdirectories = [d for d in os.listdir(source_directory) if os.path.isdir(os.path.join(source_directory, d))]
-print(f"Moving the files from {source_directory} to {destination_directory}")
-for subdirectory in tqdm(subdirectories):
-    shutil.move(os.path.join(source_directory, subdirectory), destination_directory)
-shutil.rmtree(source_directory)
-print("Images Moved")
+# source_directory = 'Datasets/VOS/train/train/JPEGImages/'
+# destination_directory = 'Datasets/VOS/train/images/'
+# os.makedirs(destination_directory)
+# subdirectories = [d for d in os.listdir(source_directory) if os.path.isdir(os.path.join(source_directory, d))]
+# print(f"Moving the files from {source_directory} to {destination_directory}")
+# for subdirectory in tqdm(subdirectories):
+#     shutil.move(os.path.join(source_directory, subdirectory), destination_directory)
+# shutil.rmtree(source_directory)
+# print("Images Moved")
 
 
-source_directory = 'Datasets/VOS/train/train/annotations/'
-destination_directory = 'Datasets/VOS/train/labels/'
-os.makedirs(destination_directory)
-print(f"Moving the files from {source_directory} to {destination_directory}")
-subdirectories = [d for d in os.listdir(source_directory) if os.path.isdir(os.path.join(source_directory, d))]
-for subdirectory in tqdm(subdirectories):
-    shutil.move(os.path.join(source_directory, subdirectory), destination_directory)
-shutil.rmtree(source_directory)
-print("Labels moved.")
+# source_directory = 'Datasets/VOS/train/train/annotations/'
+# destination_directory = 'Datasets/VOS/train/labels/'
+# os.makedirs(destination_directory)
+# print(f"Moving the files from {source_directory} to {destination_directory}")
+# subdirectories = [d for d in os.listdir(source_directory) if os.path.isdir(os.path.join(source_directory, d))]
+# for subdirectory in tqdm(subdirectories):
+#     shutil.move(os.path.join(source_directory, subdirectory), destination_directory)
+# shutil.rmtree(source_directory)
+# print("Labels moved.")
 
-shutil.rmtree('Datasets/VOS/train/train')
-
-
+# shutil.rmtree('Datasets/VOS/train/train')
 
 
-'''Now we just generate a segmentation mask.'''
 
-path = "Datasets/VOS/train/labels/"
-savepath = "Datasets/VOS/train/masks/"
-folders = os.listdir(path)
 
-print(len(folders))
+# '''Now we just generate a segmentation mask.'''
 
-for folder in tqdm(folders):
-    files = os.listdir(f'{path}{folder}')
-    os.makedirs(f'{savepath}/{folder}')
-    for unitfile in files:
-        filepath = f'{path}{folder}/{unitfile}'
-        img = np.array(Image.open(filepath).convert("RGB"))
-        old_color = np.array([236, 95, 103])
-        new_color = np.array([255, 255, 255])
-        mask = np.all(img == old_color, axis=-1)
-        img[mask] = new_color
-        img[~mask] = 0
-        img = Image.fromarray(img, mode="RGB")
-        img.save(f"{savepath}/{folder}/{unitfile}.png")
+# path = "Datasets/VOS/train/labels/"
+# savepath = "Datasets/VOS/train/masks/"
+# folders = os.listdir(path)
+
+# print(len(folders))
+
+# for folder in tqdm(folders):
+#     files = os.listdir(f'{path}{folder}')
+#     os.makedirs(f'{savepath}/{folder}')
+#     for unitfile in files:
+#         filepath = f'{path}{folder}/{unitfile}'
+#         img = np.array(Image.open(filepath).convert("RGB"))
+#         old_color = np.array([236, 95, 103])
+#         new_color = np.array([255, 255, 255])
+#         mask = np.all(img == old_color, axis=-1)
+#         img[mask] = new_color
+#         img[~mask] = 0
+#         img = Image.fromarray(img, mode="RGB")
+#         img.save(f"{savepath}/{folder}/{unitfile}.png")
+
+
+import os
+import csv
+
+# Specify the paths of the "test_images" and "test_masks" folders
+image_path = "Datasets/Driving_Dataset/test_images/"
+mask_path = "Datasets/Driving_Dataset/test_masks/"
+
+# Get the lists of files in the "test_images" and "test_masks" folders
+image_files = os.listdir(image_path)
+mask_files = os.listdir(mask_path)
+
+# Open the CSV file for writing
+with open("data_test_car.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+
+
+    # Iterate through the files and write the file paths to the CSV file
+    for i in range(min(len(image_files), len(mask_files))):
+        image_file = os.path.join(image_path, image_files[i])
+        mask_file = os.path.join(mask_path, mask_files[i])
+        writer.writerow([image_file, mask_file])
+
+
+# Specify the paths of the "test_images" and "test_masks" folders
+image_path = "Datasets/Driving_Dataset/train_images/"
+mask_path = "Datasets/Driving_Dataset/train_masks/"
+
+# Get the lists of files in the "test_images" and "test_masks" folders
+image_files = os.listdir(image_path)
+mask_files = os.listdir(mask_path)
+
+# Open the CSV file for writing
+with open("data_train_car.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    
+    # Iterate through the files and write the file paths to the CSV file
+    for i in range(min(len(image_files), len(mask_files))):
+        image_file = os.path.join(image_path, image_files[i])
+        mask_file = os.path.join(mask_path, mask_files[i])
+        writer.writerow([image_file, mask_file])
---
+++ train_SegmentationNetwork.py
@@ -0,0 +1,110 @@
+import random
+
+import torch
+import torch.optim as optim
+from torchvision import transforms
+from tqdm import tqdm
+
+from dataset import DataLoader
+from FCT import FCT
+from metric import DiceLoss, JaccardScore
+from tensorboardX import SummaryWriter 
+
+
+class Trainer():
+
+    def __init__(self, model_name, pretrained=False) -> None:
+        self.model_name = model_name
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.network = self.load_network(pretrained)
+        self.batch_size = 8
+
+    def load_network(self, pretrained):
+        if self.model_name == "focusnet":
+            model = FCT()
+            if pretrained:
+                check = torch.load('saved_model/model_focusnet.tar')
+                model.load_state_dict(check['model_state_dict'])
+        else:
+            model = UNet(in_channels=3, out_channels=1)
+            if pretrained:
+                check = torch.load('saved_model/road_model_unet.tar')
+                model.load_state_dict(check['model_state_dict'])
+        model = model.to(self.device)
+        return model
+    
+
+    def save_sample(self, epoch, x, y, y_pred):
+        path = f'Training Sneakpeeks/unet/{epoch}'
+        elements = [x, y, y_pred]
+        elements = [transforms.ToPILImage()(torch.squeeze(element[0:1, :, :, :])) for element in elements]
+
+        elements[0] = elements[0].save(f"{path}_input.jpg")
+        elements[1] = elements[1].save(f"{path}_actual.jpg")
+        elements[2] = elements[2].save(f"{path}_predicted.jpg")
+
+
+    def train(self, epochs, lr=0.0001):
+        print("Loading Datasets...")
+        train_dataloader, test_dataloader = DataLoader().load_data(self.batch_size)
+        print("Dataset Loaded... initializing parameters...")
+        model = self.network
+        optimizer = optim.AdamW(model.parameters(), lr)
+        dsc_loss = DiceLoss()
+        writer = SummaryWriter(log_dir="logs")        
+        loss_train, loss_test = [], []
+        start = 1
+        epochs = epochs+1
+        print(f"Starting to train for {epochs} epochs.")
+
+        for epoch in range(start, epochs):
+            _loss_train, _loss_test, _measure = 0, 0, 0
+            print(f"Training... at Epoch no: {epoch}")
+            num = random.randint(0, 300)
+            for i, (x, y) in enumerate(tqdm(train_dataloader)):
+                x, y = x.to(self.device), y.to(self.device)
+                optimizer.zero_grad()
+                y_pred = model(x)
+                loss = dsc_loss(y_pred, y)
+                _loss_train += loss.item()
+                loss.backward()
+                optimizer.step()
+                if i == num:
+                    self.save_sample(epoch, x, y, y_pred)
+
+            
+            if epoch%20 == 0:
+                print(f'Evaluating the performace of {epoch} epoch.')
+                for i, (x, y) in enumerate(tqdm(test_dataloader)):
+                    x, y = x.to(self.device), y.to(self.device)
+                    y_pred = model(x)
+                    # loss = crossentloss(y_pred, y)
+                    loss = dsc_loss(y_pred, y)
+                    measure = iou(y_pred, y)
+                    _measure += measure.item()
+                    _loss_test += loss.item()
+
+            writer.add_scalar("Testing Loss", _loss_test, epoch)
+            writer.add_scalar("Training Loss", _loss_train, epoch)
+            
+            loss_train.append(_loss_train)
+            loss_test.append(_loss_test)
+
+            print(f"Epoch: {epoch+1}, Training loss: {_loss_train}, Testing Loss: {_loss_test} || Jaccard Score : {_measure}")
+
+            if loss_train[-1] == min(loss_train):
+                print('Saving Model...')
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss_train
+                }, f'saved_model/road_model_unet.tar')
+            print('\nProceeding to the next epoch...')
+
+
+    
+
+model = "focusnet"
+seg = Trainer(model, pretrained=False)
+seg.train(epochs=70) 
