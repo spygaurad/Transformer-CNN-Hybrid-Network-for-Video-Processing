0caf42e3afd2d2cdfdbd25c699ef698ea49fb1aa
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-02-15 15:19:32 +0545

    Now we extract the downloaded file to a particular folder

--- makeDataset.py
+++ makeDataset.py
@@ -7,7 +7,16 @@ import numpy as np
 import shutil
 import csv
 
-# '''First we download the youtube VOS dataset from the Google Drive's link provided'''
+'''First we download the youtube VOS dataset from the Google Drive's link provided'''
+
 file_id = "13Eqw0gVK-AO5B-cqvJ203mZ2vzWck9s4"
 output_file = "file.zip"
 gdown.download(f"https://drive.google.com/uc?id={file_id}", output_file, quiet=False)
+
+
+'''Now we extract the downloaded file to a particular folder'''
+destination_directory = 'Datasets/VOS/train'
+os.makedirs(destination_directory)
+with zipfile.ZipFile(output_file, "r") as zip_ref:
+    zip_ref.extractall(destination_directory)
+    
