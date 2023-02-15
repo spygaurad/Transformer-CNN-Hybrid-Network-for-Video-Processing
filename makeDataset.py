0284b43bc62217c27707051d952aba843ece6ce5
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-02-15 15:25:36 +0545

    # source_directory = 'Datasets/VOS/train/train/JPEGImages/

--- makeDataset.py
+++ makeDataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import shutil
 import csv
 
-'''First we download the youtube VOS dataset from the Google Drive's link provided'''
+# First we download the youtube VOS dataset from the Google Drive's link provided
 print("Downloading the dataset in zip format...")
 file_id = "13Eqw0gVK-AO5B-cqvJ203mZ2vzWck9s4"
 output_file = "file.zip"
@@ -15,7 +15,7 @@ gdown.download(f"https://drive.google.com/uc?id={file_id}", output_file, quiet=F
 print("Dataset Downloaded!")
 
 
-'''Now we extract the downloaded file to a particular folder'''
+# Now we extract the downloaded file to a particular folder
 print("Extracting data...")
 destination_directory = 'Datasets/VOS/train'
 try:
@@ -26,3 +26,9 @@ except:
 with zipfile.ZipFile(output_file, "r") as zip_ref:
     zip_ref.extractall(destination_directory)
 print("Dataset extracted and stored into a folder")
+
+
+# Now we make our folder's structure, compatible with how our dataloader class does it.
+source_directory = 'Datasets/VOS/train/train/JPEGImages/'
+destination_directory = 'Datasets/VOS/train/images/'
+os.makedirs(destination_directory)
