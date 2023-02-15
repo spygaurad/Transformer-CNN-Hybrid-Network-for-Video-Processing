fdd3ed9aeb996212a4d80b1e34b9c0ffd5a9dd34
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-02-15 15:17:23 +0545

    added code to download the files

--- makeDataset.py
+++ makeDataset.py
@@ -6,3 +6,8 @@ from tqdm import tqdm
 import numpy as np
 import shutil
 import csv
+
+# '''First we download the youtube VOS dataset from the Google Drive's link provided'''
+file_id = "13Eqw0gVK-AO5B-cqvJ203mZ2vzWck9s4"
+output_file = "file.zip"
+gdown.download(f"https://drive.google.com/uc?id={file_id}", output_file, quiet=False)
