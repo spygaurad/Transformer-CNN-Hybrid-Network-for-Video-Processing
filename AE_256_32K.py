53c43f2ad79c0448f91e9ceb51d78535849a35d7
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-02-25 07:55:32 +0545

    changed some basic training settings

--- AE_256_32K.py
+++ AE_256_32K.py
@@ -341,7 +341,7 @@ def train(epochs, batch_size=16, lr=0.0001):
 
 
 
-error = train(61, batch_size=8)
+error = train(51, batch_size=16)
 # if error:
 #     torch.cuda.empty_cache()
 #     train(epochs=61, batch_size=4)
--- Full_Model.py
+++ Full_Model.py
@@ -47,7 +47,7 @@ DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 encoderdecoder = Autoencoder32K(outputType="image")
-encoderdecoder.load_state_dict(torch.load('saved_model/autoencoder_32K_VOS_60.tar')['model_state_dict'])
+encoderdecoder.load_state_dict(torch.load('saved_model/autoencoder_32K_VOS_50.tar')['model_state_dict'])
 # for params in encoderdecoder.parameters():
 #     params.requires_grad = False
 
