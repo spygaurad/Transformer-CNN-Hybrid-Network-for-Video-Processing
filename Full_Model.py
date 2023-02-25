a214f2c109be6c4039661ddddde694436a74f54c
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-02-25 11:02:19 +0545

    just testing the full transformer

--- Full_Model.py
+++ Full_Model.py
@@ -196,7 +196,7 @@ def train(epochs, lr=1e-6):
 
     print(f"Using {DEVICE} device.")
     print("Loading Datasets...")
-    train_data = DataloaderSequential(csv_file="data_sequential_VOS.csv", batch_size=BATCH_SIZE, imageSize=256).load_data()
+    train_data = DataloaderSequential(csv_file="data_sequential_VOS.csv", batch_size=BATCH_SIZE, imageSize=256).load_images()
     print("Dataset Loaded.")
     print("Initializing Parameters...")
 
