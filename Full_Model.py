9191d869265d27165737793084d0449e1678fe91
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-10 10:59:50 +0545

    making the positional encoding unlearnable again

--- Full_Model.py
+++ Full_Model.py
@@ -154,9 +154,22 @@ class VideoSegmentationNetwork(nn.Module):
         return image_preds
 
 
+    # def __get_positional__tensor(x, embedding_dim=EMBEDDED_DIMENSION):
+    #     pos_embedding = nn.Parameter(torch.randn(SEQUENCE_LENGTH, EMBEDDED_DIMENSION, requires_grad=True, device=DEVICE))
+    #     pos_tensor = torch.cat([pos_embedding[i].repeat(CHUNK_LENGTH, 1) for i in range(pos_embedding.shape[0])], dim=0)
+    #     return pos_tensor
     def __get_positional__tensor(x, embedding_dim=EMBEDDED_DIMENSION):
-        pos_embedding = nn.Parameter(torch.randn(SEQUENCE_LENGTH, EMBEDDED_DIMENSION, requires_grad=True, device=DEVICE))
-        pos_tensor = torch.cat([pos_embedding[i].repeat(CHUNK_LENGTH, 1) for i in range(pos_embedding.shape[0])], dim=0)
+    # Calculate the positional encoding matrix
+        pos_embedding = torch.zeros(SEQUENCE_LENGTH, EMBEDDED_DIMENSION, device=DEVICE)
+        for pos in range(SEQUENCE_LENGTH):
+            for i in range(embedding_dim):
+                if i % 2 == 0:
+                    pos_embedding[pos, i] = math.sin(pos / (10000 ** (i / embedding_dim)))
+                else:
+                    pos_embedding[pos, i] = math.cos(pos / (10000 ** ((i - 1) / embedding_dim)))
+        
+        # Repeat the positional encoding matrix CHUNK_LENGTH times for each element in the batch
+        pos_tensor = torch.cat([pos_embedding.repeat_interleave(CHUNK_LENGTH, dim=0) for _ in range(x.shape[1])], dim=1)
         return pos_tensor
 
 
