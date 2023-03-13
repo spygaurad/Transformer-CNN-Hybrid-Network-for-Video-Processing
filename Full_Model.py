dab44d3e564715d0f77055efe9998751946afbb3
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-13 11:09:12 +0545

    adding the loss per epoch on tensorboard

--- Full_Model.py
+++ Full_Model.py
@@ -265,7 +265,7 @@ def train(epochs, lr=1e-6):
             if i%num==0 and epoch%5==0: 
                 [__save_sample__(epoch+1, image[j], imagePred[j], str(j+1)) for j in range(SEQUENCE_LENGTH)]
 
-        writer.add_scalar("Training Loss", _loss, i)
+        writer.add_scalar("Training Loss", _loss, epoch)
 
         with open(f'trainLogs/Training_Full_Model.txt', 'w') as f:
             f.write(str(_loss) + '\n')
