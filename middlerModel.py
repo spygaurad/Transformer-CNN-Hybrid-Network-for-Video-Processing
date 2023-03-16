4918b74b29c0507c535a35db1e73fed16acd2104
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-16 12:50:50 +0545

    taking the loss of 2 frames in the sequence

--- middlerModel.py
+++ middlerModel.py
@@ -200,7 +200,7 @@ def train(epochs, lr=1e-6):
 
             # MS-SSIM loss + MSE Loss for model evaluation
 
-            loss = mseloss(imagePred, image[0:1])
+            loss = mseloss(imagePred, image[0:2])
 
             #getting the loss's number
             _loss += loss.item()
