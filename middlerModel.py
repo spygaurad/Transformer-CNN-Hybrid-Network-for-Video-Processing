f08d16d3f23eefb7c4fb61c4afc95ef5e16e3ecc
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-27 14:37:27 +0545

    implementing the transformer decoder

--- middlerModel.py
+++ middlerModel.py
@@ -136,7 +136,7 @@ class VideoSegmentationNetwork(nn.Module):
         #splitting the latents to source and target for the transformer decoder
         # src = latents[:, :256, :]
         src = latents[:, :256, :]
-        tgt = latents[:, 256:, :]
+        tgt = latents[:, 256:320, :]
         # src = torch.cat((latents[:, :256, :], torch.zeros((BATCH_SIZE, CHUNK_LENGTH, EMBEDDED_DIMENSION), device=DEVICE)), dim=1)
         # tgt = torch.cat((torch.zeros((BATCH_SIZE, CHUNK_LENGTH*(SEQUENCE_LENGTH-1), EMBEDDED_DIMENSION), device=DEVICE), latents[:, 192:256, :]), dim=1)
         # tgt = latents[:, 256:320, :]
