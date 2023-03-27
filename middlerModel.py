be448a630d5a77418dee0c0fbb5bf7113e7da3eb
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-27 14:48:51 +0545

    implementing the transformer decoder

--- middlerModel.py
+++ middlerModel.py
@@ -62,7 +62,7 @@ class Transformer_Encoder(nn.Module):
 class Transformer_Decoder(nn.Module):
     def __init__(self, output_dim, hidden_dim, num_layers, num_heads, dropout):
         super().__init__()
-        decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_dim, nhead=num_heads, dim_feedforward=hidden_dim, dropout=dropout)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=output_dim, nhead=num_heads, dim_feedforward=hidden_dim, dropout=dropout)
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
         # self.fc_out = nn.Linear(hidden_dim, output_dim)
         # self.dropout = nn.Dropout(dropout)
