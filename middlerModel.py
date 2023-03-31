24eaa3a44ccd350e8671aaa92df1eb8714f1c9a9
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-03-31 13:54:36 +0545

    changing the model to transformer decoder once

--- middlerModel.py
+++ middlerModel.py
@@ -142,21 +142,26 @@ class VideoSegmentationNetwork(nn.Module):
         zero_indices = torch.randperm(latents.shape[1])[:num_zeros]
         latents[:, zero_indices, :] = 0
 
-        #add the positional embedding
-        latents += self.positions
+        # Create a mask tensor of shape (batch_size, sequence_length)
+        mask = torch.ones((batch_size, sequence_length), dtype=torch.bool)
 
-        mask = torch.ones(latents.shape[0], latents.shape[1], latents.shape[1])
+        # Set the last 64 elements of each sequence to False to mask them
+        mask[:, -64:] = False
 
-        # Set the attention mask for the last 64 elements of each sequence to 0,
-        # so they will be ignored by the Transformer encoder
-        mask[:, 256:, :] = 0
+        # Create a causal mask tensor for the last 64 elements
+        causal_mask = torch.tril(torch.ones((64, 64), dtype=torch.bool))
 
-        # Create a triangular mask for the attention weights for the last 64 elements of each sequence
-        triangular_mask = torch.tril(torch.ones(latents.shape[1], latents.shape[1]), diagonal=-256)
-        triangular_mask = triangular_mask.unsqueeze(0).repeat(latents.shape[0], 1, 1)
+        # Repeat the causal mask tensor for each batch
+        causal_mask = causal_mask.repeat(batch_size, 1, 1)
 
-        attention_mask = mask.unsqueeze(1) * triangular_mask.unsqueeze(-1)
+        # Concatenate the two mask tensors along the sequence length dimension
+        mask = torch.cat((mask.unsqueeze(-1), causal_mask.unsqueeze(-1)), dim=-1)
 
+        # Expand the mask tensor to shape (batch_size, 1, sequence_length, sequence_length+64)
+        mask = mask.unsqueeze(1).expand(batch_size, 1, sequence_length, sequence_length+64)
+
+        # Convert the mask tensor to a float tensor and negate it to create the attention mask
+        attention_mask = (~mask).type(torch.float)
         latents_pred = self.transenc(latents, mask=attention_mask)
 
 
