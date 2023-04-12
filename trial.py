6bb99c0a41e4b8338b7efb2ca85dc7d244ba3c02
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-04-12 14:17:56 +0545

    changes in causal mask in trial

--- trial.py
+++ trial.py
@@ -2,12 +2,13 @@ import torch
 
 batch_size = 1
 sequence_length = 8
+n_elements_to_mask = 3
 
 # Create a mask tensor of shape (batch_size, sequence_length)
 mask = torch.ones((batch_size, sequence_length))
 
 # Set the last 3 elements of each sequence to False to mask them
-mask[:, -3:] = 0
+mask[:, -:] = 0
 
 # Create a causal mask tensor for the last 64 elements
 causal_mask = torch.tril(torch.ones((5, 5)))
