e8c539a894d8faa1b02f602ae4b9fcd781595bde
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-04-18 14:05:53 +0545

    finalziing the unified transformer model

--- middlerModel.py
+++ middlerModel.py
@@ -263,7 +263,7 @@ def train(epochs, lr=1e-6):
 
     #loading the model
     model = VideoSegmentationNetwork().to(DEVICE)
-    # model.load_state_dict(torch.load('saved_model/transformer_full_model_16K_10.tar')['model_state_dict'])
+    model.load_state_dict(torch.load('saved_model/Transformer_Training_16K_5seq_512D_noise_bertmask_100.tar')['model_state_dict'])
 
     #initializing the optimizer for transformer
     optimizerTransformer = optim.AdamW(model.transenc.parameters(), lr)
