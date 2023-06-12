8cefaa70a127adcc1705e80ba5e11c358ea559fb
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-06-12 11:37:37 +0545

    augmented image size changed

--- Model.py
+++ Model.py
@@ -48,7 +48,7 @@ class Model():
                 if random.random() > 0.7:
                     aug_image = trF.hflip(aug_image)
                 # Apply noise to the image, 100% of the time
-                aug_image = aug_image + torch.randn(image.size()).to(DEVICE) * 0.05 + 0.0
+                aug_image = aug_image + torch.randn(aug_image.size()).to(DEVICE) * 0.05 + 0.0
                 # Create 2-5 16x16 blackout patches in the image, along random locations in the axis of height and width
                 if random.random() > 0.5:
                     for _ in range(random.randint(0, 3)):
